b414d9bd96f63fb51f65033d330cb789312d03e9
moha_01 <mm7.mansour@gmail.com>
2026-03-02 23:41:02 -0800

    updated code, almost done with 3rd task

---
+++ src/components/solution-seo/PillarSeoStrategy.tsx
@@ -0,0 +1,29 @@
+
+export default function PillarSeoStrategy() {
+  return (
+    <section className="w-full flex justify-center items-center mb-8">
+      <div className="w-[1280px] h-[1864px] flex flex-col justify-center items-center gap-[80px]">
+        
+        <div className="w-[768px] h-[96px] flex flex-col text-center gap-[24px]">
+          <div className="w-[768px] h-[48px]">
+            <span className="w-[588.25px] h-[48px] text-[48px] font-extrabold text-[#0F172A]">
+              Our 3-Pillar SEO Strategy
+            </span>
+          </div>
+          <div className="w-[768px] h-[24px]">
+            <span className="w-[684.67px] h-[24px] font-normal text-[16px] text-[#475569]">
+              We don&apos;t just optimize code; we build a digital presence that converts browsing into bookings.
+            </span>
+          </div>
+        </div>
+
+        <div className="w-[1280px] h-[1864px] flex flex-col gap-[80px] pt-[96px] pb-[96px] pr-[160px] pl-[160px]">
+
+        </div>
+
+        <div></div>
+
+      </div>
+    </section>
+  )
+}
